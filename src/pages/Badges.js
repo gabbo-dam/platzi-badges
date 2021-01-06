cac2f599ba699fd18200b7ae34225c8c6bc79f6b
Gabriel Morales <gabbomorales1@gmail.com>
2021-01-05 19:26:07 -0600

    Fixed server

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -98,7 +98,7 @@ class Badges extends React.Component {
 
   render() {
     if (this.state.loading === true) {
-      return <Loading color='#000' type='spinningBubbles'></Loading>
+      return <Loading color="#000" type="spinningBubbles"></Loading>
     }
 
     if (this.state.error) {
@@ -108,25 +108,25 @@ class Badges extends React.Component {
     console.log('2/4. render()')
     return (
       <React.Fragment>
-        <div className='Badges'>
-          <div className='Badges__hero'>
-            <div className='Badges__container'>
+        <div className="Badges">
+          <div className="Badges__hero">
+            <div className="Badges__container">
               <img
-                className='Badges_conf-logo'
+                className="Badges_conf-logo"
                 src={confLogo}
-                alt='Conf Logo'
+                alt="Conf Logo"
               />
             </div>
           </div>
         </div>
-        <div className='Badge__container'>
-          <div className='Badges__buttons'>
-            <Link to='/badges/new' className='btn btn-primary'>
+        <div className="Badge__container">
+          <div className="Badges__buttons">
+            <Link to="/badges/new" className="btn btn-primary">
               New Badge
             </Link>
           </div>
-          <div className='Badges__container'>
-            <BadgesList badges={this.state.data} />
+          <div className="Badges__container">
+            {/* <BadgesList badges={this.state.data} /> */}
           </div>
         </div>
       </React.Fragment>
